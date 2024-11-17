6fc692628eb3c5475f08436e6aa1b5ab6fa6adae
Paul Sas <paulgaming4k@gmail.com>
2024-11-17 23:38:49 +0100

    Projektstruktur Angepasst
    Packages im JavaFX
    + Projekt KD

--- JavaOnlineShop/src/main/java/com/example/javaonlineshop/VerkaueferController.java
+++ JavaOnlineShop/src/main/java/com/example/javaonlineshop/VerkaueferController.java
@@ -3,7 +3,7 @@ package com.example.javaonlineshop;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class VerkaueferController {
     @FXML
     private Label welcomeText;
 
