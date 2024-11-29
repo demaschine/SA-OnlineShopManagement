f8a4121038555459d4da181685cf0417ee6b7287
Ultral1x <felix@c-hempel.de>
2024-11-29 18:55:04 +0100

    Update Main.java
    Superclass

--- Onlineshoptestproject/src/Main.java
+++ Onlineshoptestproject/src/Main.java
@@ -5,3 +5,49 @@ public class Main {
 
 
 }
+// Datei: src/Produkt.java
+public class Produkt {
+    private double einkaufspreis;
+    private double verkaufspreis;
+    private int bestandsmenge;
+
+    // Konstruktor
+    public Produkt(double einkaufspreis, double verkaufspreis, int bestandsmenge) {
+        this.einkaufspreis = einkaufspreis;
+        this.verkaufspreis = verkaufspreis;
+        this.bestandsmenge = bestandsmenge;
+    }
+
+    // Getter und Setter
+    public double getEinkaufspreis() {
+        return einkaufspreis;
+    }
+
+    public void setEinkaufspreis(double einkaufspreis) {
+        this.einkaufspreis = einkaufspreis;
+    }
+
+    public double getVerkaufspreis() {
+        return verkaufspreis;
+    }
+
+    public void setVerkaufspreis(double verkaufspreis) {
+        this.verkaufspreis = verkaufspreis;
+    }
+
+    public int getBestandsmenge() {
+        return bestandsmenge;
+    }
+
+    public void setBestandsmenge(int bestandsmenge) {
+        this.bestandsmenge = bestandsmenge;
+    }
+
+    // ToString-Methode (optional, aber praktisch)
+    @Override
+    public String toString() {
+        return "Einkaufspreis: " + einkaufspreis +
+                ", Verkaufspreis: " + verkaufspreis +
+                ", Bestandsmenge: " + bestandsmenge;
+    }
+}
