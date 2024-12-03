ceee272fa53b45df42c3a4a28f23470e5c86d62c
Paul Sas <paulgaming4k@gmail.com>
2024-12-03 22:57:38 +0100

    JavaDoc für die Backendstruktur
    Nach der Überarbeitung folgt die JavaDoc für die komplette Strukur, sowie Anpassungen in der Namensgebung

--- Onlineshoptestproject/src/Freizeitartikel.java
+++ Onlineshoptestproject/src/Freizeitartikel.java
@@ -1,22 +1,41 @@
+/**
+ * Diese Klasse stellt den Bauplan für Produkte der Kategorie: Freizeitartikel.
+ * <p>Hierbei handelt es sich um eine Subklasse, die von der Klasse Produkt erbt</p>
+ *
+ * @author Team Einfach.Zuahuse, Maximilian Mohr, Paul Sas
+ * @version 1.0
+ */
+
 public class Freizeitartikel extends Produkt {
     private String spieler;
 
-    //Konstruktor mit der Implementierung der super Methode um alle Attribute der Superklasse zu erben
+    /**
+     * Konstruktor der die Datenstruktur aus der Produktklasse für Freizeitartikel erweitert.
+     *
+     * @code super() ruft den Konstruktor der abstrakten Produktmethode, welche eine Produktstruktur generell vorgibt.
+     * @param spieler Das Freizeitartikel-Spezifische Attribut um Informationen über die mögliche Spieleranzahl eines Produktes dazustellen.
+     */
     public Freizeitartikel(int id,String name, double einkaufspreis, double verkaufspreis, int bestandsmenge, String spieler) {
         super(id, name, einkaufspreis, verkaufspreis, bestandsmenge);
         this.spieler = spieler;
     }
 
-    //Getter und Setter für Spieler
+    /**@return Gibt die Spieleranzahl eines Freizeitartikels zurück*/
     public String getSpieler() {
         return spieler;
     }
 
+    /**@param spieler Zugriffsmethode um auf das Attribut der Spieleranzahl eines Produktes zuzugreifen*/
     public void setSpieler(String spieler) {
         this.spieler = spieler;
     }
 
-    //ToString Methode fuer die Subklasse, uebernimmt die Attribute und die toString Methode der abstrakten Superklasse
+    /**
+     * @return Folgende ToString-Methode implementiert die ToString-Methode aus der Superklasse, um die Parameter:
+     * ProduktID, Artikelbezeichnung, Einkaufspreis, Verkaufspreis, Bestandsmenge
+     * aus der vorgegebenen Struktur als ein String. Dazu kommt noch die Information über die Spieleranzahl
+     * im gleichen Format
+     */
     @Override
     public String toString() {
         return super.toString() + ", Spieleranzahl: " + spieler;
--- Onlineshoptestproject/src/Produkt.java
+++ Onlineshoptestproject/src/Produkt.java
@@ -1,12 +1,36 @@
+/**
+ * Diese Klasse gilt der Generalisierung der Struktur, für die im Onlineshop angebotenen Produkte.
+ * Angebotene Produkte müssen für die Datenbankstruktur diesen Aufbau über die super() Methode implementieren.
+ *
+ * <p>Diese Klasse verfügt über folgende Attribute: ID, Name, Einkaufspreis, Verkaufspreis.
+ * Darüber hinaus stellt diese eine Zugriffsstruktur mithilfe von Getter und Setter, für die Datenkapselung da.</p>
+ *
+ * @author Team Einfach.Zuhause, Felix Hempel, Paul Sas
+ * @version 1.0
+ */
+
 public abstract class Produkt {
     //Private Modifier, Einbindung des Prinzips der Datenkapselung - Getter Setter fuer Attribute
+    /**ProduktID für die Datenbank-Strukur*/
     private int id;
+    /**Produktbezeichnung, weitergabe an Frontend*/
     private String name;
+    /**Einkaufspreis, zu welchem, ein gegebenes Produkt erworben wird*/
     private double einkaufspreis;
+    /**Verkaufspreis, zu welchem das gegebene Produkt im Shop angeboten wird*/
     private double verkaufspreis;
+    /**Das Attribut der Bestandsmenge gibt darüber auskunft, wie viele Entitäten eines Produktes im Lager vorhanden sind*/
     private int bestandsmenge;
 
-    //Konstruktor, der von den Subklassen vererbt werden soll
+    /**
+     * Konstruktor, der von den Subklassen vererbt werden soll
+     *
+     * @param id ProduktID
+     * @param name Name eines Produkts
+     * @param einkaufspreis Preis des Produkterwerbs
+     * @param verkaufspreis Preis des Produktvertriebs
+     * @param bestandsmenge Attribut der Lagermenge eines bestimmten Produktes mit eindeutiger ProduktID
+     */
     public Produkt(int id, String name, double einkaufspreis, double verkaufspreis, int bestandsmenge) {
         this.id = id;
         this.name = name;
@@ -15,47 +39,53 @@ public abstract class Produkt {
         this.bestandsmenge = bestandsmenge;
     }
 
+    /**@return Gibt die ProduktID zurück*/
     public int getId() {
         return id;
     }
+    /**@param id Zugriffsmethode um die ProduktID manuell zu setzen*/
     public void setId(int id) {
         this.id = id;
     }
+    /**@return Gibt die Produktbezeichnung zurück*/
     public String getName() {
         return name;
     }
+    /**@param name Zugriffsmethode um den Namen eines Produktes zu beschreiben*/
     public void setName(String name) {
         this.name = name;
     }
-
+    /**@return Gibt den Einkaufspreis eines Produktes zurück*/
     public double getEinkaufspreis() {
         return einkaufspreis;
     }
-
+    /**@param einkaufspreis Zugriffsmethode um auf den Einkaufspreis eines Produktes abzuändern*/
     public void setEinkaufspreis(double einkaufspreis) {
         this.einkaufspreis = einkaufspreis;
     }
-
+    /**@return Gibt den Verkaufspreis eines Produktes zurück*/
     public double getVerkaufspreis() {
         return verkaufspreis;
     }
-
+    /**@param verkaufspreis Zugriffsmethode um áuf den Verkaufspreis abzuändern*/
     public void setVerkaufspreis(double verkaufspreis) {
         this.verkaufspreis = verkaufspreis;
     }
 
+    /**@return bestandsmenge gibt die Bestandsmenge eines Produkts zurück*/
     public int getBestandsmenge() {
         return bestandsmenge;
     }
 
+    /**@param bestandsmenge Zugriffsmethode um die Bestandsmenge eines Produkts zu verändern*/
     public void setBestandsmenge(int bestandsmenge) {
         this.bestandsmenge = bestandsmenge;
     }
 
-    //Methode, die Werte des Objektes beim Objektaufruf strukturiert wiedergibt
+    /**@return Folgende ToString-Methode setzt den Aufbau der Rückgabe eines Produktes. Wird von Subklassen vererbt*/
     @Override
     public String toString() {
-        return "ID_PK: " + id +
+        return "ID_PK (ProduktID): " + id +
                 ", Artikelbezeichnung: " + name +
                 ", Einkaufspreis: " + einkaufspreis +
                 ", Verkaufspreis: " + verkaufspreis +
