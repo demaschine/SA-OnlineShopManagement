23d6697406c485b46d31ae5b807d46e63d009d9c
Paul Sas <paulgaming4k@gmail.com>
2024-12-19 21:50:14 +0100

    Sprint Stable 4

--- JavaOnlineShop/src/main/java/de/barm/jos_ArtikelBackend/Main.java
+++ JavaOnlineShop/src/main/java/de/barm/jos_ArtikelBackend/Main.java
@@ -3,6 +3,7 @@ package de.barm.jos_ArtikelBackend;
 import de.barm.jos_Datenbankanbindung.SQL;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
@@ -34,58 +35,70 @@ public class Main {
 //
 //        System.out.println("Programm Beendet");
 
-        Freizeitartikel test = new Freizeitartikel(1001,"testFreizeit1","", 69.0, 420.0, 1337, 100);
-        Bueroartikel test1 = new Bueroartikel(1002, "testBuero1","", 69.0,420.0, 1337, 100);
-        Kuechenartikel test2 = new Kuechenartikel(1003, "testBuero1","", 69.0,420.0, 1337, "Schwarz-Rosa-Gold");
+        Kuechenartikel test1 = new Kuechenartikel(1004, "testBuero2","", 69.0,420.0, 1337, "Schwarz-Rosa-Gold");
+        Kuechenartikel test2 = new Kuechenartikel(1005, "testBuero3","", 69.0,420.0, 1337, "Schwarz-Rosa-Gold");
+        Kuechenartikel test3 = new Kuechenartikel(1006, "testBuero4","", 69.0,420.0, 1337, "Schwarz-Rosa-Gold");
 
-        SQL.clearDBProdukte();
+        Freizeitartikel test4 = new Freizeitartikel(1007, "testFrei1","",70.0,420.0,1333,100);
+        Freizeitartikel test5 = new Freizeitartikel(1008, "testFrei2","",70.0,420.0,1333,100);
+        Freizeitartikel test6 = new Freizeitartikel(1009, "testFrei3","",70.0,420.0,1333,100);
 
-        SQL.addArtikel(test);
+        Bueroartikel test7 = new Bueroartikel(1010, "testKuche1", "", 50.0, 420.0, 222, 20);
+        Bueroartikel test8 = new Bueroartikel(1011, "testKuche2", "", 50.0, 420.0, 222, 20);
+        Bueroartikel test9 = new Bueroartikel(1012, "testKuche3", "", 50.0, 420.0, 222, 20);
+
         SQL.addArtikel(test1);
         SQL.addArtikel(test2);
+        SQL.addArtikel(test3);
+        SQL.addArtikel(test4);
+        SQL.addArtikel(test5);
+        SQL.addArtikel(test6);
+        SQL.addArtikel(test7);
+        SQL.addArtikel(test8);
+        SQL.addArtikel(test9);
 
-        test.setName("testFreizeit1 Hier könnte IHRE Werbung stehen");
 
-        SQL.updateArtikel(test);
 
        //Test für die SQL Logik, damit ich mich daran orientieren kann.
 
-        ArrayList<Produkt> produktListe = new ArrayList<Produkt>();
+  //      ArrayList<Produkt> produktListe = new ArrayList<Produkt>();
 
-        for (Produkt produkt : SQL.getArtikel()) {
-            try{
-                produktListe.add(produkt);
-                System.out.println("Produkt in CacheListe geladen");
-            }catch (Exception e){
-                e.printStackTrace();
-            }
-        }
+    //    for (Produkt produkt : SQL.getArtikel()) {
+        //      try{
+      //          produktListe.add(produkt);
+        //      System.out.println("Produkt in CacheListe geladen");
+            //  }catch (Exception e){
+            //  e.printStackTrace();
+            //  }
+        //  }
 
         //Gebe das Produkt nach einem Parameter für die ID aus
-        Produkt gefundenesProdukt = null;
+    //      Produkt gefundenesProdukt = null;
 
-        for (Produkt produkt : SQL.getArtikel()) {
+        /**
+         * for (Produkt produkt : SQL.getArtikel()) {
             if (produkt.getId() == 1001) {
                 gefundenesProdukt = produkt;
                 break;
             }
         }
-        System.out.println(gefundenesProdukt);
+        */
+        //    System.out.println(gefundenesProdukt);
 
-        System.out.println("Artikelanzahl in der SQL-Datenbank: " + SQL.getArtikel().length);
+        System.out.println("Artikelanzahl in der SQL-Datenbank: " + Objects.requireNonNull(SQL.getArtikel()).length);
 
         //User-Tests
 
         //System.out.println(SQL.addUser("testUser1", 1234, false));
-        System.out.println(SQL.updateUserPIN("testUser1", 1234));
-        System.out.println(SQL.updateUserVendor("testUser1", 1234, false));
-        System.out.println(SQL.authUser("testUser1", 1234));
-        System.out.println(SQL.authVendor("testUser1", 1234));
-        System.out.println(SQL.updateUserVendor("testUser1", 1234, true));
-        System.out.println(SQL.authUser("testUser1", 1234));
-        System.out.println(SQL.authVendor("testUser1", 1234));
-        System.out.println(SQL.updateUserPIN("testUser1", 1235));
-        System.out.println(SQL.authUser("testUser1", 1234));
-        System.out.println(SQL.authVendor("testUser1", 1234));
+        //   System.out.println(SQL.updateUserPIN("testUser1", 1234));
+        //   System.out.println(SQL.updateUserVendor("testUser1", 1234, false));
+        //   System.out.println(SQL.authUser("testUser1", 1234));
+        //    System.out.println(SQL.authVendor("testUser1", 1234));
+        //   System.out.println(SQL.updateUserVendor("testUser1", 1234, true));
+        //   System.out.println(SQL.authUser("testUser1", 1234));
+        //   System.out.println(SQL.authVendor("testUser1", 1234));
+        //   System.out.println(SQL.updateUserPIN("testUser1", 1235));
+        //    System.out.println(SQL.authUser("testUser1", 1234));
+        //    System.out.println(SQL.authVendor("testUser1", 1234));
     }
 }
